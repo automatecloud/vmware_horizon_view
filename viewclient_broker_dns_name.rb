026899e51391c7b179958832b282bb32914fa137
Andulla <andreas.wilke@puppetlabs.com>
2015-10-28 17:20:05 +0100

    new fact

---
+++ viewclient_broker_dns_name.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+#
+# author: Andreas Wilke
+#
+# Written to send the current View Client Broker DNS name to the master.
+#
+Facter.add('viewclient_broker_dns_name') do
+  confine :kernel => :windows
+
+  viewclient_broker_dns_name = 'unknown'
+  begin
+
+    if RUBY_PLATFORM.downcase.include?('mswin') or RUBY_PLATFORM.downcase.include?('mingw32')
+      require 'win32/registry'
+
+      Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\VMware, Inc.\VMware VDM\Agent\Configuration') do |reg|
+        reg.each do |name,key|
+		    if name =~ /Broker/
+            viewclient_broker_dns_name = name
+          end
+        end
+      end
+    end
+  rescue
+
+  end
+
+  setcode do
+    viewclient_broker_dns_name
+  end
+end
