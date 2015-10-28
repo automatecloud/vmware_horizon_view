1e156f1aa565e2e0be2b0f5f0ac42033087b2dd4
Andulla <andreas.wilke@puppetlabs.com>
2015-10-28 12:04:33 +0100

    Now using the right environment variable
    %VDM_StartSession_Broker_DNS_Name%

--- lib/facter/connectionserver.rb
+++ lib/facter/connectionserver.rb
@@ -1,6 +1,6 @@
 Facter.add(:connectionserver) do
   confine :kernel => 'windows'
   setcode do
-    Facter::Core::Execution.exec('echo %windir%')
+    Facter::Core::Execution.exec('echo %VDM_StartSession_Broker_DNS_Name%')
   end
 end
